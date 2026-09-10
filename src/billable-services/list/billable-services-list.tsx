9b14a5168c0531ea6bd77578c0e17f6df007e67e
Felix Maiko <maikofelix89@gmail.com>
2026-09-10 14:59:47 +0300

    (feat) Add search functionality for billable drugs and services

--- src/billable-services/list/billable-services-list.tsx
+++ src/billable-services/list/billable-services-list.tsx
@@ -1,16 +1,38 @@
-import React from 'react';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@carbon/react';
+import React, { useMemo, useState } from 'react';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow, TextInput } from '@carbon/react';
 import { type BillableService } from '../../shared/types';
-
+import styles from './billable-services-list.scss';
 interface billableServiceProps {
   billableServices: BillableService[];
 }
 const BillableServicesList: React.FC<billableServiceProps> = ({ billableServices }) => {
+  const [searchTerm,setSearchTerm] = useState<string>('');
+  const filteredBillableServicesList = useMemo(()=> filterBillableServices(searchTerm),[searchTerm,billableServices]);
   if (!billableServices || billableServices.length === 0) {
     return <>No Billable service</>;
   }
+   function filterBillableServices(searchString: string): BillableService[]{
+        if(!searchString){
+               return billableServices;
+        }
+        return billableServices.filter((bs)=>{
+            return bs?.display?.toLowerCase().trim().includes(searchString.toLowerCase().trim())
+        });
+   }
   return (
     <>
+     <div className={styles.listLayout}>
+     <div className={styles.listHeader}>
+        <div className={styles.listSearch}>
+            <TextInput
+                id="billable-service-search"
+                labelText="Search"
+                onChange={(e) => setSearchTerm(e?.target.value)}
+            />
+        </div>
+    </div>
+    <div className={styles.listContent}>
+      <div className={styles.listData}>
       <Table size="lg">
         <TableHead>
           <TableRow>
@@ -24,8 +46,8 @@ const BillableServicesList: React.FC<billableServiceProps> = ({ billableServices
           </TableRow>
         </TableHead>
         <TableBody>
-          {billableServices &&
-            billableServices.map((bs, index) => {
+          {filteredBillableServicesList &&
+            filteredBillableServicesList.map((bs, index) => {
               return (
                 <>
                   <TableRow key={bs.uuid}>
@@ -46,6 +68,9 @@ const BillableServicesList: React.FC<billableServiceProps> = ({ billableServices
             })}
         </TableBody>
       </Table>
+      </div>
+      </div>
+      </div>
     </>
   );
 };
